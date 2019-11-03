1afbaf10438e257f4eafd888ce25dee696b29ded
Taylor <tcour10@gmail.com>
2019-11-03 14:17:01 -0600

    Added Categories and Products to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'customers/index'
-  get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   resources :orders, only: %i[index show]
   resources :customers, only: %i[index show]
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[show]
 
   root to: 'admin/dashboard#index'
 end
