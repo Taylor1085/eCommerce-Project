79207a247dbca423148103901d3c3254f0d86943
Taylor <tcour10@gmail.com>
2019-11-03 15:22:39 -0600

    Seeds commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
+# frozen_string_literal: true
+
+Product.destroy_all
+Category.destroy_all
+AdminUser.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+prods = Product.create(name: 'stick', description: '99 flex hockey stick', price: 99.99)
+
+cat = Category.create(name: 'Equipment')
+
+puts Product.count
+puts Category.count
