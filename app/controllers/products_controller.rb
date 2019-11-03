d4ecab7ea2f6bf4377bc9b9e6ad615e5b38af38a
Taylor <tcour10@gmail.com>
2019-11-03 14:31:54 -0600

    Configured Product and Category controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @products = Product.find(params[:id])
   end
 end
